57eb2a2ff5b908ad02e3cbb799be278bb98a915a
jcdr428 <56721609+jcdr428@users.noreply.github.com>
2020-02-27 20:08:51 +0200

    Update tsMuxer.cpp

--- tsMuxer/tsMuxer.cpp
+++ tsMuxer/tsMuxer.cpp
@@ -1181,7 +1181,7 @@ void TSMuxer::buildPMT()
     tsPacket->setPID(DEFAULT_PMT_PID);
     tsPacket->dataExists = 1;
     tsPacket->payloadStart = 1;
-    uint32_t size = m_pmt.serialize(m_pmtBuffer + TSPacket::TS_HEADER_SIZE, 3864, !m_bluRayMode, m_m2tsMode);
+    uint32_t size = m_pmt.serialize(m_pmtBuffer + TSPacket::TS_HEADER_SIZE, 3864, !m_bluRayMode);
     uint8_t* pmtEnd = m_pmtBuffer + TSPacket::TS_HEADER_SIZE + size;
     uint8_t* curPos = m_pmtBuffer + TS_FRAME_SIZE;
     for (; curPos < pmtEnd; curPos += TS_FRAME_SIZE)
